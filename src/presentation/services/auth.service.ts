75bc238b2f2ed6196881413e91c5f62bab5f9a15
Irene Olmos Gomez <ireneog_72@hotmail.es>
2025-09-30 23:02:01 +0200

    encriptación de la contraseña

--- src/presentation/services/auth.service.ts
+++ src/presentation/services/auth.service.ts
@@ -1,3 +1,4 @@
+import { bcryptAdapter } from "../../config";
 import { UserModel } from "../../data";
 import { CustomError, RegisterUserDto, UserEntity } from "../../domain";
 
@@ -12,9 +13,11 @@ export class AuthService{
 
         try {
             const user=new UserModel(registerUserDto);
-            await user.save();
+            
 
             //todo: encriptar la contraseña
+            user.password=bcryptAdapter.hash(registerUserDto.password);
+            await user.save();
 
             //todo: generar un JWT para autenticar el usuario
 
