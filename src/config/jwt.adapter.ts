d1f272668bc1da3c63962329263bf9a70a713ac8
Irene Olmos Gomez <ireneog_72@hotmail.es>
2025-10-01 00:35:22 +0200

    token JWT seed

--- src/config/jwt.adapter.ts
+++ src/config/jwt.adapter.ts
@@ -1,12 +1,16 @@
+import { envs } from "./envs";
+
 //import jwt from "jsonwebtoken";
 var jwt = require('jsonwebtoken');
+
+const JWT_SEED=envs.JWT_SEED;
 
 export class JwtAdapter{
 
     static async generateToken(payload:any,duration:string='2h'){
 
         return new Promise((resolve)=>{
-            jwt.sign(payload,"SEED",{expiresIn:duration},(err:Error,token:string)=>{
+            jwt.sign(payload,JWT_SEED,{expiresIn:duration},(err:Error,token:string)=>{
                 if(err) return resolve(null);
                 resolve(token);
             });
