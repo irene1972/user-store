a26a6702f9dbeac7bccb459399f3da8662751885
Irene Olmos Gomez <ireneog_72@hotmail.es>
2025-09-30 23:31:27 +0200

    login usuario

--- src/presentation/services/auth.service.ts
+++ src/presentation/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { bcryptAdapter } from "../../config";
 import { UserModel } from "../../data";
-import { CustomError, RegisterUserDto, UserEntity } from "../../domain";
+import { CustomError, LoginUserDto, RegisterUserDto, UserEntity } from "../../domain";
 
 export class AuthService{
 
@@ -36,6 +36,22 @@ export class AuthService{
 
         }
 
+    }
+
+    public async loginUser(loginUserDto:LoginUserDto){
+
+        const user=await UserModel.findOne({email:loginUserDto.email});
+        if(!user) throw CustomError.badRequest('Email not exist');
+
+        const isMatching=bcryptAdapter.compare(loginUserDto.password,user.password);
+        if(!isMatching) throw CustomError.badRequest('Password is not valid');
+
+        const {password,...userEntity}=UserEntity.fromObject(user);
+
+        return {
+            user:userEntity,
+            token:'ABC'
+        }
     }
 
 }
