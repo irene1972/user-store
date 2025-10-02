a94746e56cc5dd0ad304d72bbe214f927fff7aac
Irene Olmos Gomez <ireneog_72@hotmail.es>
2025-10-02 08:19:54 +0200

    ruta y controlador del upload files

---
+++ src/presentation/file-upload/controller.ts
@@ -0,0 +1,35 @@
+import { Response, Request } from 'express';
+import { CustomError } from '../../domain';
+
+export class FileUploadController {
+
+  // DI
+  constructor(
+    //private readonly categoryService: CategoryService,
+  ) { }
+
+
+  private handleError = ( error: unknown, res: Response ) => {
+    if ( error instanceof CustomError ) {
+      return res.status( error.statusCode ).json( { error: error.message } );
+    }
+
+    console.log( `${ error }` );
+    return res.status( 500 ).json( { error: 'Internal server error' } );
+  };
+
+
+  uploadFile = ( req: Request, res: Response ) => {
+
+    res.json('upload file');
+
+  };
+
+  uploadMultipleFiles = ( req: Request, res: Response ) => {
+
+    res.json('upload file multiple');
+
+  };
+
+
+}
---
+++ src/presentation/file-upload/routes.ts
@@ -0,0 +1,22 @@
+import { Router } from 'express';
+import { FileUploadController } from './controller';
+
+
+export class FileUploadRoutes {
+
+
+  static get routes(): Router {
+
+    const router = Router();
+    const controller=new FileUploadController();
+    
+    // Definir las rutas
+    router.post('/single/:type', controller.uploadFile );
+    router.post('/multiple/:type', controller.uploadMultipleFiles );
+
+    return router;
+  }
+
+
+}
+
