bc6d65264ad9685ed83c026d4f8c052d8ff29534
Irene Olmos Gomez <ireneog_72@hotmail.es>
2025-10-02 10:25:20 +0200

    colocar archivo en subdirectorios

--- src/presentation/file-upload/controller.ts
+++ src/presentation/file-upload/controller.ts
@@ -20,18 +20,20 @@ export class FileUploadController {
     return res.status( 500 ).json( { error: 'Internal server error' } );
   };
 
-
   uploadFile = ( req: Request, res: Response ) => {
 
-    const files=req.files;
+    const type=req.params.type;
+    const validTypes=['users','products','categories'];
+    if(!validTypes.includes(type))
+      throw res.status(400).json({error:`Invalid argument type: ${type}, valid ones: ${validTypes}`});
 
-    if(!files || Object.keys(files).length===0){
+    if(!req.files || Object.keys(req.files).length===0){
       return res.status(400).json({error:'No files were selected'});
     }
   
-    const file=files.file as UploadedFile;
+    const file=req.files.file as UploadedFile;
 
-    this.fileUploadService.uploadSingle(file)
+    this.fileUploadService.uploadSingle(file,`uploads/${type}`)
       .then(uploaded=>res.json(uploaded))
       .catch(error=>this.handleError(error,res));
     //console.log(req.files);
